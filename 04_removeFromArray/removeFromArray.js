ccd2ceb3e99db4b8433e8901400b3b0c86a7ed4f
Brandon Personal <blg1209@gmail.com>
2022-06-20 16:02:10 -0500

    Example of filter and forEach

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,19 @@
-const removeFromArray = function() {
+const removeFromArray = function(...toRemove) {
+   /* const array = toRemove[0];
+    return array.filter(item => !toRemove.includes(item));
+*/
+let array = toRemove[0];
+let newlist = [];
+
+array.forEach((element) => {
+    if (!toRemove.includes(element)){
+        newlist.push(element);
+    }
+});
+return newlist
 
 };
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
