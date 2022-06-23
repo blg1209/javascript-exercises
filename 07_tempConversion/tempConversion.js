1ab49c1dced88290c024aa14b3c9feef04d4530b
Brandon Personal <blg1209@gmail.com>
2022-06-22 21:03:52 -0500

    Working temp conversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,16 @@
-const ftoc = function() {
+const ftoc = function(far) {
+  let convertedCelsius =  (far - 32) * (5/9);
+  return Math.round(convertedCelsius *10)/10
 
 };
 
-const ctof = function() {
+const ctof = function(cel) {
+  let convertedFarenheit = (cel * (9/5) + 32);
+  return Math.round(convertedFarenheit *10)/10
 
 };
 
+console.log(ftoc(100))
 // Do not edit below this line
 module.exports = {
   ftoc,
